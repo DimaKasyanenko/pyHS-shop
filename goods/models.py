c81872e83a8af814f23b2ab709405eba150ba59f
DimaKasyanenko <98147501+DimaKasyanenko@users.noreply.github.com>
2024-07-23 17:53:26 +0300

    Add model Category

--- goods/models.py
+++ goods/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, unique=True, verbose_name="Название категории")
+    slug = models.SlugField(max_length=250, unique=True, blank=True, null=True, verbose_name="Ссылка на категорию")
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+    def __str__(self):
+        return self.name
