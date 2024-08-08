14ed5f9dfc8c6a5341dc09ea1ce9c906f39457a2
DimaKasyanenko <98147501+DimaKasyanenko@users.noreply.github.com>
2024-08-08 14:24:32 +0300

    Добавлено отображение по категориям

--- goods/views.py
+++ goods/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from goods.models import Product
 
 
-def catalog(request):
-    goods = Product.objects.all()
+def catalog(request, category_slug):
+    if category_slug == 'all':
+        goods = Product.objects.all()
+    else:
+        goods = get_object_or_404(Product.objects.filter(category__slug=category_slug))
+
     context = {
         'title': 'Home - Каталог',
         'goods': goods,
@@ -15,8 +19,10 @@ def catalog(request):
 
 def product(request, product_slug):
     product = Product.objects.get(slug=product_slug)
+
     context = {
         'title': product.name,
         'product': product,
     }
+
     return render(request, 'goods/product.html', context)
